222026a94bb346d139a67e5b2b8aefb57d87c3a4
ankitranasd <71355563+ankitranasd@users.noreply.github.com>
2024-02-22 14:35:34 +0530

    Update application.py

--- application.py
+++ application.py
@@ -79,6 +79,6 @@ def index():
     else:
         return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+#if __name__ == "__main__":
+   # app.run(host='127.0.0.1', port=5000, debug=True)
 	#app.run(debug=True)
